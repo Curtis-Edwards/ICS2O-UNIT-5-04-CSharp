a0ee8236368f5a235f124feff22fb6e920d213c5
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-04 21:29:13 +0000

    fixed indenting

--- main.cs
+++ main.cs
@@ -2,23 +2,31 @@ using System;
 
 class Program
 {
-    public static void Main(string[] args)
-    {
+   public static void Main(string[] args)
+   {
         // This function accepts user input
-        int radius;
-        double pi = Math.PI;
-        double area;
+        int age;
+        int weekday;
 
-        Console.WriteLine("Please enter the radius of the circle");
+        Console.WriteLine("Please enter your age and the day of the week");
         Console.WriteLine("");
 
-        Console.Write("radius: ");
-        radius = Convert.ToInt32(Console.ReadLine());
+        Console.Write("age: ");
+        age = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
-        area = (pi * radius) * radius;
+        Console.Write("day of the week: ");
+        weekday = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
 
-        Console.WriteLine("Area: " + area.ToString("0.00") + "cm²");
+        if ((weekday == 2 || weekday == 4) || (age >= 12 && age <= 21))
+        {
+            Console.WriteLine("You are eligible for student pricing!");
+        }
+        else
+        {
+            Console.WriteLine("You must pay regular pricing.");
+        }
 
         Console.WriteLine("\nDone.");
-    }
+   }
 }
